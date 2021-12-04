5268264a42a79f5d919d20f38d781fab22abac55
Yeren Palacios <yeren.palacios@omnilatam.com>
2021-12-03 23:35:25 -0500

    Created header component

---
+++ src/components/header/header.js
@@ -0,0 +1,19 @@
+import logo from "../../logo.svg"
+import "./header.css"
+import { Link } from "react-router-dom"
+
+function Header({actual}){
+
+    return(
+        <header>
+            <p className={actual=="projects"? "actual": null}><Link to="/projects">projects</Link></p>
+            <p className={actual=="portfolio"? "actual": null}><Link to="/portfolio">portfolio</Link></p>
+            <div className="logo">
+                <img src={logo}/>
+            </div>
+            <p className={actual==""? "contact": null}><Link to="/contact">Contact</Link></p>
+            <p className={actual==""? "about": null}><Link to="/about">About</Link></p>
+        </header>
+    )
+}
+export default Header
